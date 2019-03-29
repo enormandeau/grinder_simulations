536d0cf2d24d2b4926450a34b355a4f591da8ea6
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-03-29 10:57:08 -0400

    Change default quality values of good and error nucleotides

--- 01_scripts/utility_scripts/run_one_grinder_sample.sh
+++ 01_scripts/utility_scripts/run_one_grinder_sample.sh
@@ -14,7 +14,7 @@ source "$CONFIG_FILE"
 # Running one instance of Grinder on SLURM
 grinder -rf "$INPUT_SEQUENCES" \
     -tr "$NUM_SEQUENCES" -am powerlaw "$POWERLAW_EXPONENT" \
-    -id "$INSERT_DIST" -rd "$READ_LENGTHS" -fq 1 -ql 30 20 -mo FR \
+    -id "$INSERT_DIST" -rd "$READ_LENGTHS" -fq 1 -ql 37 25 -mo FR \
     -dc '-' -md poly4 3e-3 3.3e-8 -mr "$MUTATION_RATIO" \
     -hd Balzer -cp "$CHIMERA_PERCENT" -ck 0 -mi "$TAG_SEQUENCE" \
     -bn "$SAMPLE_NAME" -od "$OUTPUT_FOLDER"
