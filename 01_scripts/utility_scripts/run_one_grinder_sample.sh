9a1a8b2e2ab6fcb3a5592e1e3a2482f4e78e46af
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-04-03 15:12:43 -0400

    Modify quality scores for legibility

--- 01_scripts/utility_scripts/run_one_grinder_sample.sh
+++ 01_scripts/utility_scripts/run_one_grinder_sample.sh
@@ -14,10 +14,12 @@ source "$CONFIG_FILE"
 # Running one instance of Grinder on SLURM
 grinder -rf "$INPUT_SEQUENCES" \
     -tr "$NUM_SEQUENCES" -am powerlaw "$POWERLAW_EXPONENT" \
-    -id "$INSERT_DIST" -rd "$READ_LENGTHS" -fq 1 -ql 37 25 -mo FR \
+    -id "$INSERT_DIST" -rd "$READ_LENGTHS" -fq 1 -ql 36 30 -mo FR \
     -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr "$MUTATION_RATIO" \
     -hd Balzer -cp "$CHIMERA_PERCENT" -ck 0 -mi "$TAG_SEQUENCE" \
     -bn "$SAMPLE_NAME" -od "$OUTPUT_FOLDER"
 
 # Compress output file
 gzip "$OUTPUT_FOLDER"/"$SAMPLE_NAME"-reads.fastq
+
+sleep 2
