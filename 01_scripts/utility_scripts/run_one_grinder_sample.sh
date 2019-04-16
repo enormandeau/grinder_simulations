9af1c1d4d3e0012778edbd2dbe9118408389bf82
Eric Normandeau <eric.normandeau.qc@gmail.com>
2019-04-16 09:23:40 -0400

    Error model poly4 -> uniform

--- 01_scripts/utility_scripts/run_one_grinder_sample.sh
+++ 01_scripts/utility_scripts/run_one_grinder_sample.sh
@@ -15,7 +15,7 @@ source "$CONFIG_FILE"
 grinder -rf "$INPUT_SEQUENCES" \
     -tr "$NUM_SEQUENCES" -am powerlaw "$POWERLAW_EXPONENT" \
     -id "$INSERT_DIST" -rd "$READ_LENGTHS" -fq 1 -ql 36 30 -mo FR \
-    -dc '-' -un 1 -md poly4 3e-3 3.3e-8 -mr "$MUTATION_RATIO" \
+    -dc '-' -un 1 -md uniform 0.1 -mr "$MUTATION_RATIO" \
     -hd Balzer -cp "$CHIMERA_PERCENT" -ck 0 -mi "$TAG_SEQUENCE" \
     -bn "$SAMPLE_NAME" -od "$OUTPUT_FOLDER"
 
